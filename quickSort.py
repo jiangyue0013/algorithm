f23fbe70f5f69ce5b9a257c266e727335019d406
jiangyue0013 <jiangyue0013@gmail.com>
2019-05-22 19:02:45 +0800

    '增加注释'

--- quickSort.py
+++ quickSort.py
@@ -1,7 +1,7 @@
 def quickSort(array):
-    if len(array) < 2:
+    if len(array) < 2:  # 基线条件
         return array
-    else:
+    else:  # 递归条件
         pivot = array[0]
         less = [i for i in array[1:] if i <= pivot]
         greater = [i for i in array[1:] if i > pivot]
